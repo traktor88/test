f4e4d3c0b4287dbdbd830e14f4607d133589d278
DrMartiner <DrMartiner@GMail.Com>
2021-03-08 11:46:28 +0200

    Fixed #193 -- Corrected a bad reference to ValidationError. Thanks Jarek Zgoda.
    git-svn-id: http://django-tagging.googlecode.com/svn/trunk@155 83e7428b-ec2a-0410-86f2-bf466d0e5e72

--- tagging/admin.py
+++ tagging/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from tagging.models import Tag, TaggedItem, Synonym
+from tagging.models import Tag, TaggedItem
 
 admin.site.register(TaggedItem)
 admin.site.register(Tag)
-admin.site.register(Synonym)
